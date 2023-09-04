5b249d312caa4c3da76a8f8e651fe3ae96c1ec38
YEELAM <cathy@CathydeMacBook-Pro-2.local>
2023-09-04 11:38:20 +0800

    Problem 1 part c, d and e are finished.

--- PS1.R
+++ PS1.R
@@ -21,3 +21,39 @@ wine_count
 #df2
 
 #c)
+df[df$Alcohol==max(df$Alcohol),]#Extract the rows with highest alcohol content.
+df[df$Alcohol==min(df$Alcohol),]#Extract the rows with lowest alcohol content.
+count(df[df$Magnesium>114, ])#Extract the rows with magnesium higher than 114mg/l then count them.
+df2<-df[df$Magnesium>114, ]
+count_by_magnesium<- df2 %>% group_by(class) %>% summarise(total_count=n(),.groups = 'drop')
+count_by_magnesium
+
+#d)
+# Using apply
+all.means<-apply(df, 2, mean)
+# Using colMeans
+all.means<-colMeans(df)
+#Applying the same method for 3 different classes.
+df3<-df[df$class==1,]
+means_c1<-apply(df3, 2, mean)
+means_c1<-colMeans(df3)
+
+df4<-df[df$class==2,]
+means_c2<-apply(df4, 2, mean)
+means_c2<-colMeans(df4)
+
+df5<-df[df$class==3,]
+means_c3<-apply(df5, 2, mean)
+means_c3<-colMeans(df5)
+
+#Combining these dataframe into one table.
+avg_table =cbind.data.frame(all.means,means_c1,means_c2,means_c3)
+avg_table
+
+#e)
+#t test on level of ashes of class 1 and class 2
+t.test(df3$Ash, df4$Ash, var.equal = TRUE)
+#t test on level of ashes of class 2 and class 3
+t.test(df4$Ash, df5$Ash, var.equal = TRUE)
+#t test on level of ashes of class 1 and class 3
+t.test(df3$Ash, df5$Ash, var.equal = TRUE)
